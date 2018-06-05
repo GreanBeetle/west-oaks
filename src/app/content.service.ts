adc23e688ad87d1fd08ec950affec3444b74f88d
John Rykken <john.rykken@gmail.com>
2018-06-05 10:00:57 -0700

    Import contents.json file to firebase. Rebuild content.service.ts.

--- src/app/content.service.ts
+++ src/app/content.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Content } from './models/content.model';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 
 @Injectable()
 export class ContentService {
+  contents: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private database: AngularFireDatabase) {
+    this.contents = database.list('contents');
+  }
 
+  getContents() {
+    return this.contents;
+  }
 
 
 }
