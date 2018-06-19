64be16c73f20e44b53e3e1fc8d1112f79652ac15
John Rykken <john.rykken@gmail.com>
2018-06-19 10:34:54 -0700

    Return Observable<any[]> by year.

--- src/app/minutes/minutes.component.ts
+++ src/app/minutes/minutes.component.ts
@@ -17,21 +17,21 @@ import { Router } from '@angular/router';
 export class MinutesComponent implements OnInit {
   minutesArray: AngularFirestoreCollection<any>;
   minutes: Observable<any[]>;
-  array2017: AngularFirestoreCollection<any>;
-  arr2017: Observable<any[]>;
-  array2016: AngularFirestoreCollection<any>;
-  arr2016: Observable<any[]>;
+  monthsArray2017: AngularFirestoreCollection<any>;
+  months2017: Observable<any[]>;
+  monthsArray2016: AngularFirestoreCollection<any>;
+  months2016: Observable<any[]>;
 
   constructor(private afs: AngularFirestore, public authService: AuthenticationService, private router: Router) {
     this.minutesArray = afs.collection<any>('minutes', ref => ref.orderBy('year', 'desc'));
     this.minutes = this.minutesArray.valueChanges();
+    this.monthsArray2017 = afs.collection<any>('minutes', ref => ref.where('year', '==', 2017));
+    this.months2017 = this.monthsArray2017.valueChanges();
+    console.log('2017 is this: ' + this.months2017);
+    this.monthsArray2016 = afs.collection<any>('minutes', ref => ref.where('year', '==', 2016));
+    this.months2016 = this.monthsArray2016.valueChanges();
+    console.log('2016 is this: ' + this.months2016);
 
-    this.itemCollection = this.afs.collection<any>('minutes', ref => {
-     // Compose a query using multiple .where() methods
-     return ref
-             .where('year', '==', '2017');
-    });
-    this.items = this.itemCollection.valueChanges();
   }
 
   ngOnInit() {
