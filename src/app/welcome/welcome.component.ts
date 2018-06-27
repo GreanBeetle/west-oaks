8e0e42b1944e30a26bbb4547d51ccd51395f8945
John Rykken <john.rykken@gmail.com>
2018-06-27 10:22:21 -0700

    Make minor stylistic improvements to contact section.

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Ng4TwitterTimelineService } from 'ng4-twitter-timeline/lib/index';
-
-
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
+
 export class WelcomeComponent {
   @Input() meeting;
-
   constructor(private ng4TwitterTimelineService: Ng4TwitterTimelineService) { }
 }
